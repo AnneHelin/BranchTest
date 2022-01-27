e3e27b983ad4b2016597a8f69872e1fd7479c426
anne <43341@edu.raseko.fi>
2022-01-27 15:01:18 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+# PÄÄOHJELMA
+
+# KIRJASTOJEN JA MODULIEN LATAUS
+
+# Otetaan käyttöön Windows-äänikirjasto (pythonin sisälle rakennettu)
+import winsound
+
+# Otetaan käyttöön oma funktiomoduuli
+import xfunktio
